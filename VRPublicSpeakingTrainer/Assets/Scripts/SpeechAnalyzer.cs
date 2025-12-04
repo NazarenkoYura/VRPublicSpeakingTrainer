bef9f99ffe63d858c39ae0f848f74c80f491ddc7
baumankavr <baumankavr@gmail.com>
2025-12-04 18:32:57 +0300

    4.12.2025

--- VRPublicSpeakingTrainer/Assets/Scripts/SpeechAnalyzer.cs
+++ VRPublicSpeakingTrainer/Assets/Scripts/SpeechAnalyzer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d438a0435b2aa297c9d0c47439c49b222d0ef68559c042e9a1bd909a3f86e7b0
-size 6886
+oid sha256:c9da4a572bc05144e4a7c399ef6628353daa959b02f18af0f5ed24ec319d2d9b
+size 6894
