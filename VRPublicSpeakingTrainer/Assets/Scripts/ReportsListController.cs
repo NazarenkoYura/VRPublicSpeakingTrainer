ebd1c79f70cfa37c15d3ff90b4bef67b0bb2e81d
baumankavr <baumankavr@gmail.com>
2025-12-09 19:29:50 +0300

    ВСЁ! СДЕЛАНО!
    Все требования ТЗ выполнены в полном объёме

--- VRPublicSpeakingTrainer/Assets/Scripts/ReportsListController.cs
+++ VRPublicSpeakingTrainer/Assets/Scripts/ReportsListController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f6e9ec8864951d357024c04d3d2fb2bdf3b3543d015b52f617f6c6f38baa52c
-size 4419
+oid sha256:f4ff69907e3be2d1522ad605c39baf7c41b4f1a0fb8d05dfe55518b1ca89df93
+size 4421
